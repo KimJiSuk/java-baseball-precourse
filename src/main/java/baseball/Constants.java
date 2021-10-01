1b1326d0a83cfb5cd227bb8c397e4db7f11731eb
KimJiSuk <yes0229@gmail.com>
2021-10-01 12:40:25 +0900

    feat(view): view 생성
    print & readLine
    print message Constants로 작성

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -4,4 +4,12 @@ public final class Constants {
     public static final int NUMBER_OF_NUMBERS = 3;
     public static final int LOW_NUMBER = 1;
     public static final int HIGH_NUMBER = 9;
+
+    public static final String GAME_OVER_MESSAGE = NUMBER_OF_NUMBERS + "개의 숫자를 모두 맞히셨습니다! 게임 끝";
+    public static final String NEW_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String ENTER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String NOTHING_MESSAGE = "낫싱";
+    public static final String STRIKE_MESSAGE = "스트라이크";
+    public static final String BALL_MESSAGE = "볼";
+
 }
---
+++ src/main/java/baseball/GameView.java
@@ -0,0 +1,50 @@
+package baseball;
+
+import nextstep.utils.Console;
+
+public class GameView {
+    public void printEnterNumber() {
+        System.out.print(Constants.ENTER_NUMBER_MESSAGE);
+    }
+
+    public String readLine() {
+        return Console.readLine();
+    }
+
+    public void printBallCount(BallCountDTO ballCountDTO) {
+        if (ballCountDTO.isNothing()) {
+            printNothing();
+            return;
+        }
+
+        printStrike(ballCountDTO.getStrike());
+        printBall(ballCountDTO.getBall());
+        System.out.println();
+    }
+
+    public void printGameOverMessage() {
+        System.out.println(Constants.GAME_OVER_MESSAGE);
+        printNewGameMessage();
+    }
+
+    private void printNewGameMessage() {
+        System.out.println(Constants.NEW_GAME_MESSAGE);
+    }
+
+    private void printNothing() {
+        System.out.println(Constants.NOTHING_MESSAGE);
+    }
+
+    private void printStrike(int strike) {
+        if (strike != 0) {
+            System.out.print(strike + Constants.STRIKE_MESSAGE + " ");
+        }
+    }
+
+    private void printBall(int ball) {
+        if (ball != 0) {
+            System.out.print(ball + Constants.BALL_MESSAGE);
+        }
+    }
+
+}
